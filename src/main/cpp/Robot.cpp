da55a6f3e593dee2efc650df99ad7dba6514c914
Sasser7 <sasser7.dev@gmail.com>
2023-08-03 13:13:05 +0200

    TankDrive functionality added
    Replaced joystick with XboxController class and added TankDrive functionality

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,62 +1,76 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
-#include <numbers>
-
-// test
-
-#include <frc/Encoder.h>
 #include <frc/Joystick.h>
 #include <frc/TimedRobot.h>
-#include <frc/motorcontrol/PWMSparkMax.h>
-#include <frc/smartdashboard/SmartDashboard.h>
-#include <frc/XboxController.h>
+#include <frc/drive/DifferentialDrive.h>
+#include "rev/CANSparkMax.h"
 
-/**
- * This sample program shows how to control a motor using a joystick. In the
- * operator control part of the program, the joystick is read and the value is
- * written to the motor.
- *
- * Joystick analog values range from -1 to 1 and motor controller inputs as
- * range from -1 to 1 making it easy to work together.
- *
- * In addition, the encoder value of an encoder connected to ports 0 and 1 is
- * consistently sent to the Dashboard.
- */
 class Robot : public frc::TimedRobot {
- public:
-  void TeleopPeriodic() override { 
-    if(controller.GetRightTriggerAxis() > 0)
-      left_motor.Set(-controller.GetRightY());
-
-    if(controller.GetLeftTriggerAxis() > 0)
-      right_motor.Set(controller.GetLeftY());
-  }
-
-  /*
-   * The RobotPeriodic function is called every control packet no matter the
-   * robot mode.
+  /**
+   * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
+   * 
+   * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
+   * first parameter
+   * 
+   * The motor type is passed as the second parameter. Motor type can either be:
+   *  rev::CANSparkMax::MotorType::kBrushless
+   *  rev::CANSparkMax::MotorType::kBrushed
+   * 
+   * The example below initializes four brushless motors with CAN IDs 1, 2, 3 and 4. Change
+   * these parameters to match your setup
    */
-  void RobotPeriodic() override {
-    frc::SmartDashboard::PutNumber("Encoder", m_encoder.GetDistance());
+  static const int leftLeadDeviceID = 2, leftFollowDeviceID = 3, rightLeadDeviceID = 4, rightFollowDeviceID = 5;
+  rev::CANSparkMax m_leftLeadMotor{leftLeadDeviceID, rev::CANSparkMax::MotorType::kBrushed};
+  rev::CANSparkMax m_rightLeadMotor{rightLeadDeviceID, rev::CANSparkMax::MotorType::kBrushed};
+  rev::CANSparkMax m_leftFollowMotor{leftFollowDeviceID, rev::CANSparkMax::MotorType::kBrushed};
+  rev::CANSparkMax m_rightFollowMotor{rightFollowDeviceID, rev::CANSparkMax::MotorType::kBrushed};
+
+  /**
+   * In RobotInit() below, we will configure m_leftFollowMotor and m_rightFollowMotor to follow 
+   * m_leftLeadMotor and m_rightLeadMotor, respectively. 
+   * 
+   * Because of this, we only need to pass the lead motors to m_robotDrive. Whatever commands are 
+   * sent to them will automatically be copied by the follower motors
+   */
+  frc::DifferentialDrive m_robotDrive{m_leftLeadMotor, m_rightLeadMotor};
+
+  frc::Joystick m_stick{0};
+  
+ public:
+  void RobotInit() {
+    /**
+     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
+     * in the SPARK MAX to their factory default state. If no argument is passed, these
+     * parameters will not persist between power cycles
+     */
+    m_leftLeadMotor.RestoreFactoryDefaults();
+    m_rightLeadMotor.RestoreFactoryDefaults();
+    m_leftFollowMotor.RestoreFactoryDefaults();
+    m_rightFollowMotor.RestoreFactoryDefaults();
+    
+    /**
+     * In CAN mode, one SPARK MAX can be configured to follow another. This is done by calling
+     * the Follow() method on the SPARK MAX you want to configure as a follower, and by passing
+     * as a parameter the SPARK MAX you want to configure as a leader.
+     * 
+     * This is shown in the example below, where one motor on each side of our drive train is
+     * configured to follow a lead motor.
+     */
+    m_leftFollowMotor.Follow(m_leftLeadMotor);
+    m_rightFollowMotor.Follow(m_rightLeadMotor);
   }
 
-  void RobotInit() override {
-    // Use SetDistancePerPulse to set the multiplier for GetDistance
-    // This is set up assuming a 6 inch wheel with a 360 CPR encoder.
-    m_encoder.SetDistancePerPulse((std::numbers::pi * 6) / 360.0);
+  void TeleopPeriodic() {
+    // Drive with arcade style
+    m_robotDrive.ArcadeDrive(-m_stick.GetX(), -m_stick.GetY());
   }
-
- private:
-  frc::XboxController controller{0};
-  frc::PWMSparkMax left_motor{0};
-  frc::PWMSparkMax right_motor{1};
-  frc::Encoder m_encoder{0, 1};
 };
 
 #ifndef RUNNING_FRC_TESTS
-int main() {
-  return frc::StartRobot<Robot>();
-}
+int main() { return frc::StartRobot<Robot>(); }
 #endif
