f087334efd13db643a0885372091356c27db9043
BuildTools <unconfigured@null.spigotmc.org>
2023-08-31 16:33:49 +0200

    Zgrupowanie silników

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -10,6 +10,7 @@
 #include <frc/drive/DifferentialDrive.h>
 #include "rev/CANSparkMax.h"
 #include <frc/XboxController.h>
+#include <frc/motorcontrol/MotorControllerGroup.h>
 #include <cameraserver/CameraServer.h>
 #include <iostream>
 
@@ -27,23 +28,28 @@ class Robot : public frc::TimedRobot {
    * The example below initializes four brushless motors with CAN IDs 1, 2, 3 and 4. Change
    * these parameters to match your setup
    */
-  static const int leftLeadDeviceID = 2, leftFollowDeviceID = 3, rightLeadDeviceID = 4, rightFollowDeviceID = 5;
-  rev::CANSparkMax m_leftLeadMotor{leftLeadDeviceID, rev::CANSparkMax::MotorType::kBrushed};
-  rev::CANSparkMax m_rightLeadMotor{rightLeadDeviceID, rev::CANSparkMax::MotorType::kBrushed};
-  rev::CANSparkMax m_leftFollowMotor{leftFollowDeviceID, rev::CANSparkMax::MotorType::kBrushed};
-  rev::CANSparkMax m_rightFollowMotor{rightFollowDeviceID, rev::CANSparkMax::MotorType::kBrushed};
+  static const int leftFrontDeviceID = 2, leftRearDeviceID = 3, rightFrontDeviceID = 4, rightRearDeviceID = 5;
+  rev::CANSparkMax m_leftFrontMotor{leftFrontDeviceID, rev::CANSparkMax::MotorType::kBrushed};
+  rev::CANSparkMax m_rightFrontMotor{rightFrontDeviceID, rev::CANSparkMax::MotorType::kBrushed};
+  rev::CANSparkMax m_leftRearMotor{leftRearDeviceID, rev::CANSparkMax::MotorType::kBrushed};
+  rev::CANSparkMax m_rightRearMotor{rightRearDeviceID, rev::CANSparkMax::MotorType::kBrushed};
 
   /**
-   * In RobotInit() below, we will configure m_leftFollowMotor and m_rightFollowMotor to follow 
-   * m_leftLeadMotor and m_rightLeadMotor, respectively. 
-   * 
-   * Because of this, we only need to pass the lead motors to m_robotDrive. Whatever commands are 
-   * sent to them will automatically be copied by the follower motors
+   * In RobotInit() below, we will group left and right motors using the MotorControllerGroup class.
+   *    
+   * For this reason, we only need to pass motor groups to m_robotDrive.
+   * Whatever commands are sent to them will be automatically forwarded to the motors.
    */
-  frc::DifferentialDrive m_robotDrive{m_leftLeadMotor, m_rightLeadMotor};
 
-  //frc::Joystick m_stick{0};
-  frc::XboxController pad{0};
+  frc::MotorControllerGroup m_leftMotors{m_leftFrontMotor, m_leftRearMotor};
+  frc::MotorControllerGroup m_rightMotors{m_rightFrontMotor, m_rightRearMotor};
+
+  frc::DifferentialDrive m_robotDrive{m_leftMotors, m_rightMotors};
+
+  frc::Joystick m_stick{0};
+  frc::XboxController pad{1};
+
+  // Variable speedMulFactor is the value by which the input to the motors is multiplied
   float speedMulFactor = 1;
   
  public:
@@ -54,28 +60,18 @@ class Robot : public frc::TimedRobot {
      * in the SPARK MAX to their factory default state. If no argument is passed, these
      * parameters will not persist between power cycles
      */
-    m_leftLeadMotor.RestoreFactoryDefaults();
-    m_rightLeadMotor.RestoreFactoryDefaults();
-    m_leftFollowMotor.RestoreFactoryDefaults();
-    m_rightFollowMotor.RestoreFactoryDefaults();
-    
-    /**
-     * In CAN mode, one SPARK MAX can be configured to follow another. This is done by calling
-     * the Follow() method on the SPARK MAX you want to configure as a follower, and by passing
-     * as a parameter the SPARK MAX you want to configure as a leader.
-     * 
-     * This is shown in the example below, where one motor on each side of our drive train is
-     * configured to follow a lead motor.
-     */
-    m_leftFollowMotor.Follow(m_leftLeadMotor);
-    m_rightFollowMotor.Follow(m_rightLeadMotor);
+    m_leftFrontMotor.RestoreFactoryDefaults();
+    m_rightFrontMotor.RestoreFactoryDefaults();
+    m_leftRearMotor.RestoreFactoryDefaults();
+    m_rightRearMotor.RestoreFactoryDefaults();
 
-    m_rightLeadMotor.SetInverted(true);
+    m_rightMotors.SetInverted(true);
   }
 
-  void TeleopPeriodic() {
-    // Drive with TankDrive
+  void TeleopPeriodic()
+  {
     m_robotDrive.TankDrive(pad.GetLeftY() * speedMulFactor, pad.GetRightY() * speedMulFactor);
+
     if (pad.GetRightBumperPressed() && speedMulFactor < 1)
     {
       speedMulFactor += 0.2;
